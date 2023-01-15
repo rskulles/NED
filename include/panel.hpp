65f1d27fc92c26b49dca2fad5c885312b41bb0c1
Roy S <Roy S>
2023-01-15 14:53:17 -0500

    A little progress on a base panel implementation

--- include/panel.hpp
+++ include/panel.hpp
@@ -1,28 +1,59 @@
 #ifndef PANEL_HPP
 #define PANEL_HPP
-#include<scene_object.hpp>
+#include <scene_object.hpp>
+#include <theme.hpp>
 
-enum class PanelType{
+enum class PanelType
+{
     Stack,
     Grid,
     Scroll,
 };
 
-class Panel: public SceneObject{
+class Panel : public SceneObject
+{
 public:
+    Panel(Theme &theme, const sf::FloatRect rect_def) : drawable_()
+    {
+        set_dimensions(sf::Vector2f(rect_def.width, rect_def.height));
+        set_position(sf::Vector2f(rect_def.left, rect_def.top));
+        update_theme(theme);
+    }
 
-    auto get_type() -> ObjectType const override {
+    auto get_type() -> ObjectType const override
+    {
         return ObjectType::Panel;
     }
 
-    virtual auto update(const float &dt)->void override{
+    virtual auto update(const float &dt) -> void override
+    {
+    }
 
+    void set_dimensions(const sf::Vector2f &dimensions)
+    {
+        drawable_.setSize(dimensions);
+    }
+
+    void set_position(const sf::Vector2f &posision)
+    {
+        drawable_.setPosition(posision);
+    }
+
+    void update_theme(Theme &theme)
+    {
+        auto color = theme.panel_background_color;
+        drawable_.setFillColor(sf::Color(color.red, color.green, color.blue, color.alpha));
     }
 
 protected:
-    void draw(sf::RenderTarget &target, sf::RenderStates states) const override {
-
+    void draw(sf::RenderTarget &target, sf::RenderStates states) const override
+    {
+        states.transform = drawable_.getTransform();
+        target.draw(drawable_,states);
     }
+
+private:
+    sf::RectangleShape drawable_;
 };
 
-#endif//PANEL_HPP
+#endif // PANEL_HPP
